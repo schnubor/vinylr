362578c04a43628b78abc688b1336cb03c668e97
schnubor <schnuppser@gmail.com>
2014-01-12 22:51:14 +0100

    now adding vinyls ;)

--- js/main.js
+++ js/main.js
@@ -6,80 +6,89 @@ var Main = (function()
   // this is where stuff happens
 	function _doAfterLogin(name, fbid){
 		_addUserToDb(name, fbid);
+    _updateForms(fbid);
 		_getExistingData(fbid);
 	}
 
 	// Add username with his fb id to db
-  	function _addUserToDb(name, fbid){
-    	console.log('calling ajax with '+name+' and '+fbid);
-    	$.ajax({
-      		type: 'POST',
-      		url: './php/createuser.php',
-      		data: {
-        		username: name,
-        		facebookid: fbid
-     		},
-      		success: function (response) {
-        		console.log("record has been saved in database: "+response);
-      		},
-      		error: function () {
-        		console.log('there is some error');
-      		}
-    	});
-  	}
+	function _addUserToDb(name, fbid){
+  	console.log('calling ajax with '+name+' and '+fbid);
+  	$.ajax({
+    		type: 'POST',
+    		url: './php/createuser.php',
+    		data: {
+      		username: name,
+      		facebookid: fbid
+   		},
+    		success: function (response) {
+      		console.log("record has been saved in database: "+response);
+    		},
+    		error: function () {
+      		console.log('there is some error');
+    		}
+  	});
+	}
 
-    // After logged in, get users vinyl Data and display it on success
-  	function _getExistingData(fbid){
-      console.log("calling _getExistingData");
-  		$.ajax({
-      		type: 'POST',
-      		url: './php/getuservinyls.php',
-      		data: {
-        		facebookid: fbid
-     		},
-      		success: function (response) {
-            console.log(response);
-            if(response.length){
-              VINYLS = $.parseJSON(response);
-              _displayVinylData(VINYLS);
-            }
-      		},
-      		error: function () {
-        		console.log('there is some error');
-      		}
-    	});
-  	}
-
-    // Build actual Vinyl List and display it
-    function _displayVinylData(vinyls){
-      console.log(vinyls);
-      $('#vinylcount').text(vinyls.length);
-      index = 0;
-      $.each(vinyls, function(){
-        content = '<tr>';
-        content += '<td>'+VINYLS[index].VinylID+'</td>';
-        content += '<td>'+VINYLS[index].Artist+'</td>';
-        content += '<td>'+VINYLS[index].Album+'</td>';
-        content += '<td>'+VINYLS[index].Label+'</td>';
-        content += '<td>'+VINYLS[index].Format+' '+VINYLS[index].Type+'</td>';
-        content += '<td>'+VINYLS[index].Year+'</td>';
-        content += '<td>'+VINYLS[index].Price+' €</td>';
-        content += '<td>'+VINYLS[index].Catalog+'</td>';
-        content += '<td>'+VINYLS[index].Genre+'</td>';
-        content += '</tr>';
-        
-        $('#tablecontent').append(content);
-        index += 1;
-      });
-
+  // Update Add and Edit forms with hidden input containing FB ID
+  function _updateForms(fbid){
+    if(!$('#hiddenfbidinput').length){ // if not already present
+      $('#addvinylform').prepend('<input type="hidden" id="hiddenfbidinput" name="fbid" value="'+fbid+'">');
     }
+  }
 
-  	return{
-  		doAfterLogin: _doAfterLogin,
-  		addUserToDb: _addUserToDb,
-  		getExistingData: _getExistingData,
-      displayVinylData: _displayVinylData
-  	}
+  // After logged in, get users vinyl Data and display it on success
+	function _getExistingData(fbid){
+    console.log("calling _getExistingData");
+		$.ajax({
+    		type: 'POST',
+    		url: './php/getuservinyls.php',
+    		data: {
+      		facebookid: fbid
+   		},
+    		success: function (response) {
+          console.log(response);
+          if(response.length){
+            VINYLS = $.parseJSON(response);
+            _displayVinylData(VINYLS);
+          }
+    		},
+    		error: function () {
+      		console.log('there is some error');
+    		}
+  	});
+	}
+
+  // Build actual Vinyl List and display it
+  function _displayVinylData(vinyls){
+    console.log(vinyls);
+    $('#vinylcount').text(vinyls.length);
+    index = 0;
+    $.each(vinyls, function(){
+      content = '<tr>';
+      content += '<td>'+VINYLS[index].VinylID+'</td>';
+      content += '<td>'+VINYLS[index].Artist+'</td>';
+      content += '<td>'+VINYLS[index].Album+'</td>';
+      content += '<td>'+VINYLS[index].Label+'</td>';
+      content += '<td>'+VINYLS[index].Format+' '+VINYLS[index].Type+'</td>';
+      content += '<td>'+VINYLS[index].Year+'</td>';
+      content += '<td>'+VINYLS[index].Price+' €</td>';
+      content += '<td>'+VINYLS[index].Catalog+'</td>';
+      content += '<td>'+VINYLS[index].Genre+'</td>';
+      content += '</tr>';
+      
+      $('#tablecontent').append(content);
+      index += 1;
+    });
+
+  }
+
+	return{
+		doAfterLogin: _doAfterLogin,
+		addUserToDb: _addUserToDb,
+    updateForms: _updateForms,
+		getExistingData: _getExistingData,
+    displayVinylData: _displayVinylData
+	}
 
 })();
 
--- php/addvinyl.php
+++ php/addvinyl.php
@@ -7,7 +7,16 @@
 	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
 
-	echo 'Hello world from addvinyl.php!';
+	$sql = "INSERT INTO `$_POST[fbid]` (Artist, Album, Label, Catalog, Year, Format, Type, Price, Genre)
+	VALUES
+	('$_POST[artist]','$_POST[title]','$_POST[label]','$_POST[catalog]','$_POST[year]','$_POST[size]','$_POST[type]','$_POST[price]','$_POST[genre]')";
+
+	if (!mysqli_query($con,$sql))
+	{
+	  die('Error: ' . mysqli_error($con));
+	}
+
+	echo 'Written to DB!';
 
 	mysqli_close($con);
 ?>
